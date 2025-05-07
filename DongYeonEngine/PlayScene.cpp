190aed09a9ef49732a42bb67cb828348d7ed64c5
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-05-07 22:05:53 +0900

    적 먹이 충돌처리

--- DongYeonEngine/PlayScene.cpp
+++ DongYeonEngine/PlayScene.cpp
@@ -1,6 +1,10 @@
 #pragma once
 #include "PlayScene.h"
 #include "Input.h"
+#define ENEMY_SIZE 5
+#define TRAR_SIZE 7
+#define JUMBO_SIZE 10
+#define FOOD_SIZE 50
 // 카메라 객체는 필요 시 추가
 
 std::uniform_int_distribution<int> widthUid(0, 1280);
@@ -10,7 +14,7 @@ std::random_device rd;
 PlayScene::PlayScene()
 {
     // 음식 객체 초기화
-    for (int i = 0; i < 30; ++i)
+    for (int i = 0; i < FOOD_SIZE; ++i)
     {
         Food obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -18,7 +22,7 @@ PlayScene::PlayScene()
     }
 
     // 적 객체 초기화
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < ENEMY_SIZE; ++i)
     {
         Enemy obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -26,7 +30,7 @@ PlayScene::PlayScene()
     }
 
     // 트랩 객체 초기화
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < TRAR_SIZE; ++i)
     {
         Trap obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -34,7 +38,7 @@ PlayScene::PlayScene()
     }
 
     // 점보 객체 초기화
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < JUMBO_SIZE; ++i)
     {
         Jumbo obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -52,7 +56,7 @@ PlayScene::~PlayScene()
 void PlayScene::Initialize()
 {
     // 음식 객체 초기화
-    for (int i = 0; i < 30; ++i)
+    for (int i = 0; i < FOOD_SIZE; ++i)
     {
         Food obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -60,7 +64,7 @@ void PlayScene::Initialize()
     }
 
     // 적 객체 초기화
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < ENEMY_SIZE; ++i)
     {
         Enemy obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -68,7 +72,7 @@ void PlayScene::Initialize()
     }
 
     // 트랩 객체 초기화
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < TRAR_SIZE; ++i)
     {
         Trap obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -76,7 +80,7 @@ void PlayScene::Initialize()
     }
 
     // 점보 객체 초기화
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < JUMBO_SIZE; ++i)
     {
         Jumbo obj;
         obj.SetPosition(widthUid(rd), heightUid(rd));
@@ -91,7 +95,7 @@ void PlayScene::Update()
 {
     
 
-    // 플레이어 <-> 음식 충돌 체크 (원 기반)
+    // 플레이어 <-> 음식 충돌 체크
     for (auto it = foods.begin(); it != foods.end();)
     {
         // 중심점 간 거리 계산
@@ -115,6 +119,39 @@ void PlayScene::Update()
     }
 
     //적 <-> 먹이 충돌체크 
+    for (auto it = enemys.begin(); it != enemys.end();)
+    {
+        for (auto jt = foods.begin(); jt != foods.end();)
+        {
+            float dx = it->GetPositionX() - jt->GetPositionX();
+            float dy = it->GetPositionY() - jt->GetPositionY();
+            float distance = std::sqrt(dx * dx + dy * dy);
+
+            // 반지름 합과 비교
+            float radiusSum = it->GetRadius() + jt->GetRadius();
+
+            if (distance <= radiusSum)
+            {
+                float deltaRadius = (jt->GetRadius()/4) + it->GetRadius();
+                it->Setradius(deltaRadius);
+                jt = foods.erase(jt);
+                std::cout << it->GetSpeed() << std::endl;
+            }
+            else
+            {
+                jt++;
+            }
+        }
+        it++;
+    }
+
+    if (foods.size() <= FOOD_SIZE) //50개 고정
+    {
+        Food obj;
+        obj.SetPosition(widthUid(rd), heightUid(rd));
+        foods.push_back(obj);
+    }
+
     //플레이어 업데이트
     player.Update();
 
